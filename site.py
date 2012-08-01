0b4df06f2b893338320a8a68caf75fc0f249b483
Eivind Uggedal <eivind@uggedal.com>
2012-08-01 16:16:28 +0200

    Sort pages by date.

--- site.py
+++ site.py
@@ -1,4 +1,5 @@
 import sys
+import datetime
 
 from flask import Flask, render_template
 from flask.ext.flatpages import FlatPages, pygments_style_defs
@@ -7,17 +8,28 @@ from flask.ext.frozen import Freezer
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
+PAGE_DATE_FORMAT="%Y/%m/%d %H:%M:%S"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-pages = FlatPages(app)
 
-def get_pages():
-    return (p for p in pages if not 'draft' in p.meta)
+def parse_dates(pages):
+    for p in pages:
+        if 'date' in p.meta:
+            p.meta['date'] = datetime.datetime.strptime(p.meta['date'],
+                                                        PAGE_DATE_FORMAT)
+    return pages
+
+pages = parse_dates(FlatPages(app))
+
+def get_latest_pages():
+    latest = sorted(pages, reverse=True,
+                    key=lambda p: p.meta['date'])
+    return (p for p in latest if not 'draft' in p.meta)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pages=get_pages())
+    return render_template('index.html', pages=get_latest_pages())
 
 @app.route('/pygments.css')
 def pygments_css():
@@ -25,7 +37,7 @@ def pygments_css():
 
 @app.route('/<path:path>/')
 def page(path):
-    page = get_pages().get_or_404(path)
+    page = pages.get_or_404(path)
     return render_template('page.html', page=page)
 
 if __name__ == '__main__':
