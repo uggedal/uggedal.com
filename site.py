d06930f5e901977e92461d08318246c5311963c1
Eivind Uggedal <eivind@uggedal.com>
2012-08-01 19:49:09 +0200

    Journal index, s/page/article/.

--- site.py
+++ site.py
@@ -11,24 +11,28 @@ FLATPAGES_EXTENSION = '.md'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-pages = FlatPages(app)
+articles = FlatPages(app)
 
-def get_latest_pages():
-    return sorted((p for p in pages if not 'draft' in p.meta),
+def get_latest_articles():
+    return sorted((p for p in articles if not 'draft' in p.meta),
                   reverse=True, key=lambda p: p.meta['date'])
 
 @app.route('/')
 def index():
-    return render_template('index.html', pages=get_latest_pages()[:3])
+    return render_template('index.html', articles=get_latest_articles()[:3])
 
 @app.route('/pygments.css')
 def pygments_css():
     return pygments_style_defs('tango'), 200, {'Content-Type': 'text/css'}
 
+@app.route('/journal')
+def journal():
+    return render_template('journal.html', articles=get_latest_articles())
+
 @app.route('/<path:path>/')
-def page(path):
-    page = pages.get_or_404(path)
-    return render_template('page.html', page=page)
+def article(path):
+    article = articles.get_or_404(path)
+    return render_template('article.html', article=article)
 
 if __name__ == '__main__':
     if 'build' in sys.argv[1:]:
