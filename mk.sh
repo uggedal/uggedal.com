8874d37795e47915f7b13b5890e5cf2a6a40d280
Eivind Uggedal <eivind@uggedal.com>
2014-01-22 23:11:16 +0000

    ocd

--- mk.sh
+++ mk.sh
@@ -30,14 +30,12 @@ inject() {
   sed "${line}r $1" | sed "${line}d"
 }
 
-layout=page.tmpl
-
 site_title='Eivind Uggedal'
 title=$(header $1 1)
 date=$(header $1 2)
 export site_title title date
 
-
+layout=page.tmpl
 tmp=$(mktemp)
 trap "rm $tmp" EXIT TERM INT
 
