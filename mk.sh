a8c8059cf4f8fd17116074a366702d50e5b33c80
Eivind Uggedal <eivind@uggedal.com>
2014-01-22 22:54:57 +0000

    refac

--- mk.sh
+++ mk.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
-page_template=page.tmpl
-
 header() {
   sed $2'q;d' $1 | sed 's/^% //'
 }
+
+tmpl() {
+  awk '
+  {
+    print substitute($0)
+  }
+
+  function substitute(raw) {
+    if (match(raw, /\{\{([^}]*)\}\}/)) {
+      tag = substr(raw, RSTART, RLENGTH)
+      key = substr(raw, RSTART+2, RLENGTH-4)
+      gsub(tag, ENVIRON[key], raw)
+      return substitute(raw)
+    } else {
+      return raw
+    }
+  }
+  ' $1
+}
+
+page_template=page.tmpl
+body_line=$(sed -n '/@@BODY@@/=' $page_template)
 
 site_title='Eivind Uggedal'
 title=$(header $1 1)
@@ -17,21 +37,4 @@ trap "rm $tmp" EXIT TERM INT
 
 markdown $1 > $tmp
 
-body_line=$(sed -n '/@@BODY@@/=' $page_template)
-
-awk '
-{
-  print substitute($0)
-}
-
-function substitute(raw) {
-  if (match(raw, /\{\{([^}]*)\}\}/)) {
-    tag = substr(raw, RSTART, RLENGTH)
-    key = substr(raw, RSTART+2, RLENGTH-4)
-    gsub(tag, ENVIRON[key], raw)
-    return substitute(raw)
-  } else {
-    return raw
-  }
-}
-' $page_template | sed "${body_line}r $tmp" | sed "${body_line}d" > ${1%*.md}.html
+tmpl $page_template | sed "${body_line}r $tmp" | sed "${body_line}d" > ${1%*.md}.html
