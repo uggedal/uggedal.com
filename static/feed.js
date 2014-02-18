0fbd4297ec9752ba1197dfc98f42cd7c20e6e967
Eivind Uggedal <eivind@uggedal.com>
2014-02-18 10:54:28 +0000

    feed: display last 10 messages

--- static/feed.js
+++ static/feed.js
@@ -1,4 +1,11 @@
-function createCommitFeed() {
+function req (path, cb) {
+  var script = document.createElement('script');
+  script.src = 'https://api.github.com' + path + '?callback=' + cb;
+
+  document.head.appendChild(script);
+}
+
+function init () {
   if (document.location.pathname !== '/') return;
 
   var footer = document.querySelector('body > footer');
@@ -7,10 +14,31 @@ function createCommitFeed() {
   section.className = 'commit-feed';
   section.innerHTML = [
     '<header><h1>Latest Commits</h1></header>',
+    '<ol>',
+    '</ol>',
     '<p><a href=https://github.com/uggedal>All commits</a></p>',
   ].join('');
 
-  document.body.insertBefore(section, footer);
+  return document.body.insertBefore(section, footer);
 }
 
-createCommitFeed();
+var section = init();
+var MAX = 10;
+
+function handler (res) {
+  var i = 0;
+
+  section.querySelector('ol').innerHTML = res.data.map(function (item) {
+    if (item.type !== 'PushEvent') return '';
+
+    return item.payload.commits.map(function (commit) {
+      if (i++ >= MAX) return;
+      return '<li>' + commit.message + '</li>';
+    }).join('');
+  }).join('');
+};
+
+
+if (section) {
+  req('/users/uggedal/events/public', 'handler');
+}
